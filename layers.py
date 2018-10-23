7a167dc410504e410d8bf5f816316123fe55ce3c
Mohit Jain <mohitjain1999@yahoo.com>
2018-10-24 01:28:20 +0530

    Create transposed convolutional layer function.

--- layers.py
+++ layers.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 def convLayer(x, filter_height, filter_width, 
-    num_filters, name, stride=1, padding='SAME'):
+    num_filters, name, stride=2, padding='SAME'):
 
     """Create Convolutional Layer"""
     
@@ -17,7 +17,7 @@ def convLayer(x, filter_height, filter_width,
         b = tf.get_variable('biases', shape=[num_filters], initializer=tf.constant_initializer(0.0))
         
         #Perform convolution and add bias.
-        conv = tf.nn.conv2d(x, Wm strides=[1, stride, stride, 1], padding=padding)
+        conv = tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding=padding)
         z = tf.nn.bias_add(conv, b)
         
         #Add batch-norm layer.
@@ -27,6 +27,36 @@ def convLayer(x, filter_height, filter_width,
         out = tf.nn.relu(batch_norm)
 
         return out
+
+def transposeConvLayer(x, filter_height, filter_width,
+    num_filters, name, output_shape, stride=2, paddin='SAME'):
+
+    """Create Transposed Convolutional Layer"""
+    
+    #Get number of input channels.
+    input_channels = int(x.get_shape()[-1])
+
+    with tf.variable_scope(name) as scope:
+        
+        #Create tf variables for the weight and biases of the conv layer.
+        W = tf.get_variable('weights', shape=[filter_height, filter_width, num_filters, input_channels],
+            initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01))
+
+        b = tf.get_variable('biases', shape=[num_filters], initializer=tf.constant_initializer(0.0))
+        
+        #Perform transposed convolution and add bias.
+        tconv = tf.nn.conv2d_transpose(x, w, strides=[1, stride, stride, 1], padding=padding, output_shape=output_shape)
+        z = tf.nn.bias_add(tconv, b)
+        
+        #Add batch-norm layer
+        batch_norm = tf.layers.batch_normalization(z, axis=1, beta_initializer=tf.constant_initializer(0.0),
+            gamma_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01))
+
+        out = tf.nn.relu(batch_norm)
+
+        return out
+
+
 
 def fcLayer(x, input_size, output_size, name, relu=True):
 
