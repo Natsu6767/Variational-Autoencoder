b7883eab2419f3d9016b93887d5d57df038370d1
Mohit Jain <mohitjain1999@yahoo.com>
2018-10-24 05:14:08 +0530

    Fix errors.

--- vae.py
+++ vae.py
@@ -3,43 +3,50 @@ import tensorflow as tf
 from layers import convLayer, tConvLayer, fcLayer
 
 class VAE(object):
+    """Creating the VAE model"""
 
-	def __init__(self, x, n_z):
-		
-		self.n_z = n_z
-		self.X = x
-		X_reshaped = tf.reshape(self.X, [-1, 28, 28, 1])
+    def __init__(self, x, n_z, batch_size):
+        
+        self.n_z = n_z
+        self.X = x
+        self.batch_size = batch_size
+        X_reshaped = tf.reshape(self.X, [-1, 28, 28, 1])
 
-		z_mean, z_stddev = self.recognition(X_reshaped)
-		epsilon = tf.random_normal([self.X.get_shape()[0], self.n_z], 0, 1, dtype=tf.float32)
-		z_generated = z_mean + (tf.exp(z_stddev) * epsilon)
+        z_mean, z_stddev = self.recognition(X_reshaped)
+        epsilon = tf.random_normal(tf.stack([self.batch_size, self.n_z]))
+        self.z_generated = tf.add(z_mean, tf.multiply(tf.exp(z_stddev), epsilon))
 
-		self.generated_images = self.generation(z_generated)
+        self.generated_images = self.generation(self.z_generated)
+        generated_flatten = tf.reshape(self.generated_images, [self.batch_size, 28*28])
+        
+        #Binary cross-entropy
+        self.reconstruction_loss = -tf.reduce_sum(self.X * tf.log(1e-8 + generated_flatten) + (1-self.X) * tf.log(1e-8 + 1 - generated_flatten),1)
+        #KL Divergence loss term
+        self.latent_loss = -0.5 * tf.reduce_sum(1 + 2.0*z_stddev - tf.square(z_mean) - tf.exp(2.0*z_stddev),1)
+        self.cost = tf.reduce_mean(self.reconstruction_loss + self.latent_loss)
+    
+    #Encoder
+    def recognition(self, input_image):
 
-		generated_flatten = tf.contrib.layers.flatten(self.generated_images)
+        with tf.variable_scope("recognition"):
+            conv1 = convLayer(input_image, 5, 5, 16, "conv1")
+            conv2 = convLayer(conv1, 5, 5, 32, "conv2")
+            conv2_flat = tf.contrib.layers.flatten(conv2)
 
-		self.reconstruction_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits = generated_flatten, labels = self.X), 1)
-		self.latent_loss = 0.5 * tf.reduce_sum(1 + 2.0*z_stddev - tf.square(z_mean) - tf.exp(2.0*z_stddev) ,1)
-		self.cost = tf.reduce_mean(self.reconstruction_loss + self.latent_loss)
+            mean = fcLayer(conv2_flat, self.n_z, relu=False, name="mean")
+            stddev = fcLayer(conv2_flat, self.n_z, relu=False, name="stddev")
 
-	def recognition(self, input_image):
+        return mean, stddev
+    
+    #Decoder
+    def generation(self, z):
 
-		with tf.variable_scope("recognition"):
-			conv1 = convLayer(input_image, 5, 5, 64, "conv1")
-			conv2 = convLayer(conv1, 5, 5, 128, "conv2")
-			conv2_flat = tf.contrib.layers.flatten(conv2)
+        with tf.variable_scope("generation"):
+            z_expand = tf.reshape(fcLayer(z, 7*7*32, "z_expand"), [-1, 7, 7, 32])
 
-			mean = fcLayer(conv2_flat, self.n_z, "mean")
-			stddev = fcLayer(conv2_flat, self.n_z, "stddev")
+            tconv1 = tConvLayer(z_expand, 5, 5, [self.batch_size, 14, 14, 16], "tconv1")
+            tconv2 = tConvLayer(tconv1, 5, 5, [self.batch_size, 28, 28, 1], "tconv2", activation=False)
 
-		return mean, stddev
+            out = tf.nn.sigmoid(tconv2)
 
-	def generation(self, z):
-
-		with tf.variable_scope("generation"):
-			z_expand = tf.reshape(fcLayer(z, 7*7*128, "z_expand"), [-1, 7, 7, 128])
-
-			tconv1 = tConvLayer(z_expand, 5, 5, [int(z_expand.get_shape()[0]), 14, 14, 64], "tconv1")
-			tconv2 = tConvLayer(tconv1, 5, 5, [int(tconv1.get_shape()[0]), 28, 28, 1], "tconv2")
-
-		return tconv2
+        return out
